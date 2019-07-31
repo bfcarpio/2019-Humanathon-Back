6d9cd3346535ba6106dedd71cfd8b63579b3ec9f
Brendan Carpio <bfcarpio@gmail.com>
2019-07-30 20:32:19 -0400

    :recycle: mongo-go-driver

--- main.go
+++ main.go
@@ -1,53 +1,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/labstack/echo"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 	"log"
 	"net/http"
+
+	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // LocationSummary struct
 type LocationSummary struct {
-	ID    bson.ObjectId `json:"id" bson:"_id,omitempty"`
-	Label string        `json:"label"`
-	Description string 	`json:"description"`
+	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Label       string             `json:"label"`
+	Description string             `json:"description"`
 }
 
 // Location struct
 type Location struct {
-	ID          bson.ObjectId `json:"id" bson:"_id,omitempty"`
-	Label       string        `json:"label"`
-	Description string        `json:"description"`
-	Phone       string        `json:"phone"`
-	Map         string        `json:"map"`
-	X           int           `json:"x"`
-	Y           int           `json:"y"`
+	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Label       string             `json:"label"`
+	Description string             `json:"description"`
+	Phone       string             `json:"phone"`
+	Map         string             `json:"map"`
+	X           int                `json:"x"`
+	Y           int                `json:"y"`
 }
 
 func main() {
-	session, err := mgo.Dial("mongodb://localhost:27017")
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
+	log.Println("Starting MongoDB connection...")
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb+srv://app:humana@cluster0-5ybnu.gcp.mongodb.net/test?retryWrites=true&w=majority")
 
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
-	db := session.DB("muFind").C("Locations")
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
 
-	jacob := Location{bson.NewObjectId(), "Jacob", "Creates and Deletes projects", "555-123-4567", "Waterside_10", 34, 10}
-	ray := Location{bson.NewObjectId(), "Ray", "Anime", "555-123-4567", "Waterside_10", 32, 156}
-	ben := Location{bson.NewObjectId(), "Ben", "Lorem ipsur delor", "555-123-4567", "demo", 31, 645}
-	aaron := Location{bson.NewObjectId(), "Aaron", "Logic", "555-123-4567", "Waterside_10", 31, 56}
-	brendan := Location{bson.NewObjectId(), "Brendan", "*The* Super Chicken", "555-123-4567", "demo", 31, 329}
-	err = db.Insert(&jacob, &ray, &ben, &aaron, &brendan)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("test").Collection("locations")
 
 	e := echo.New()
 
@@ -59,13 +65,30 @@ func main() {
 	e.GET("/locations", func(e echo.Context) error {
 		fmt.Println("/locations")
 
-		loc := []LocationSummary{}
-		err := db.Find(nil).Select(bson.M{"_id": 1, "label": 1, "description": 1}).All(&loc)
+		locs := []*LocationSummary{}
+		cur, err := collection.Find(context.TODO(), bson.D{{}}, nil)
 		if err != nil {
 			log.Println("Failed to get any record")
 			return e.JSON(http.StatusNotFound, err)
 		}
-		return e.JSON(http.StatusOK, loc)
+
+		for cur.Next(context.TODO()) {
+
+			// create a value into which the single document can be decoded
+			var elem LocationSummary
+			err := cur.Decode(&elem)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			locs = append(locs, &elem)
+		}
+
+		if err := cur.Err(); err != nil {
+			log.Fatal(err)
+		}
+		cur.Close(context.TODO())
+		return e.JSON(http.StatusOK, locs)
 
 	})
 	e.GET("/locations/:id", func(e echo.Context) error {
@@ -73,7 +96,13 @@ func main() {
 		log.Println("GET /locations/", requestID)
 
 		loc := Location{}
-		err := db.FindId(bson.ObjectIdHex(requestID)).One(&loc)
+		id, err := primitive.ObjectIDFromHex(requestID)
+		if err != nil {
+			log.Fatal(err)
+			panic(err)
+		}
+
+		err = collection.FindOne(context.TODO(), bson.M{"_id": bson.M{"$eq": id}}).Decode(&loc)
 		if err != nil {
 			log.Println("Failed to find location with key: ", requestID)
 			return e.JSON(http.StatusNotFound, requestID)
@@ -90,18 +119,32 @@ func main() {
 			log.Fatal(err)
 			return e.JSON(http.StatusNotFound, err)
 		}
-		loc.ID = bson.NewObjectId()
 
-		err = db.Insert(&loc)
+		insertResult, err := collection.InsertOne(context.TODO(), loc)
 		if err != nil {
 			log.Fatal(err)
-			return e.JSON(http.StatusNotFound, err)
 		}
 
-		return e.JSON(http.StatusCreated, loc)
+		return e.JSON(http.StatusCreated, insertResult.InsertedID)
+	})
+	e.DELETE("/locations/:id", func(e echo.Context) error {
+		requestID := e.Param("id")
+		log.Println("DELETE /locations", requestID)
+
+		id, err := primitive.ObjectIDFromHex(requestID)
+		if err != nil {
+			log.Fatal(err)
+			panic(err)
+		}
+
+		deleteResult, err := collection.DeleteOne(context.TODO(), bson.M{"_id": bson.M{"$eq": id}})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return e.JSON(http.StatusOK, deleteResult)
 	})
 
 	// Start as a web server
 	e.Start(":8080")
-
 }
